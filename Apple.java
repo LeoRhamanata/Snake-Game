814c5910c7195f9591a4294a6405e7caa8aecd03
entitas-L <167918703+GhostXvestia@users.noreply.github.com>
2026-05-26 10:04:10 +0800

    Clean up Apple class by removing comments
    Removed comments and documentation from the Apple class.

--- Apple.java
+++ Apple.java
@@ -1,12 +1,5 @@
 import java.util.Random;
 
-/**
- * Class Apple - extends Entity
- * Representasi buah apel yang dimakan ular.
- * Posisi apel di-generate secara acak.
- *
- * Role 1 - Class Architect
- */
 public class Apple extends Entity {
     private static final char APPLE_SYMBOL = '@';
     private Random random;
@@ -17,11 +10,7 @@ public class Apple extends Entity {
         respawn(boardWidth, boardHeight);
     }
 
-    /**
-     * Pindahkan apel ke posisi random baru di dalam board
-     */
     public void respawn(int boardWidth, int boardHeight) {
-        // Pastikan tidak spawn di border
         this.x = 1 + random.nextInt(boardWidth - 2);
         this.y = 1 + random.nextInt(boardHeight - 2);
     }
