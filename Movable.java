6a91104de0b48209c2614803aab1a34ac2989717
entitas-L <167918703+GhostXvestia@users.noreply.github.com>
2026-05-26 10:06:48 +0800

    Update Movable.java

--- Movable.java
+++ Movable.java
@@ -1,9 +1,3 @@
-/**
- * Interface Movable
- * Diimplementasikan oleh entitas yang bisa bergerak di dalam game.
- *
- * Role 1 - Class Architect
- */
 public interface Movable {
     void move();
     void changeDirection(char direction);
